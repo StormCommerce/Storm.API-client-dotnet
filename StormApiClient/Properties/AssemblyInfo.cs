3735bbccc0143a8f2b2ed215eff67432e337f511
Joel Nygren <joel.nygren@norce.io>
2023-07-07 13:42:00 +0200

    fixed pricelist bugg

--- StormApiClient/Properties/AssemblyInfo.cs
+++ StormApiClient/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 #if DEBUG
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 #else
  [assembly: AssemblyTitle("Enferno.StormApiClient release version")]
  [assembly: AssemblyConfiguration("Release")]
- [assembly: AssemblyDescription("Release version")] 
+ [assembly: AssemblyDescription("Release version")]
 #endif
 [assembly: AssemblyCompany("Enferno AB")]
 [assembly: AssemblyProduct("Enferno.StormApiClient")]
@@ -20,8 +20,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -31,14 +31,13 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.4")]
-[assembly: AssemblyFileVersion("2.5.4")]
+[assembly: AssemblyVersion("2.5.5")]
+[assembly: AssemblyFileVersion("2.5.5")]
 [assembly: InternalsVisibleTo("Enferno.StormApiClient.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001004bcdc6bd4f8cc4bd93a2f9a8348c51c585dd1c91dc9f1c235061f24ba3c4d49adb1c5e07267028758a998aea56ba5d7b124ccd86828c676f6c9ac551abd9b51156c996e9ce0eac7bc94965ac4e81db8e9c294f9080f71b1c3fc7ae4dabec0f5304d6949201480dea3f557a76b685f98575632dc43ff788b5a0b0eeb6c17f31cd")]
-
